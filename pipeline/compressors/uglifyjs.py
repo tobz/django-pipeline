414c6c4ecc90c321a9f07ecb1e61a836246d605b
Toby Lawrence <tlawrence@edx.org>
2016-01-04 15:34:55 -0500

    Add sourcemap support to the UglifyJS compressor.

--- pipeline/compressors/uglifyjs.py
+++ pipeline/compressors/uglifyjs.py
@@ -1,7 +1,28 @@
 from __future__ import unicode_literals
 
+import os
+import re
+import shlex
+import tempfile
+
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.utils import six
+
 from pipeline.conf import settings
 from pipeline.compressors import SubProcessCompressor
+
+
+source_map_re = re.compile((
+    "(?:"
+      "/\\*"
+      "(?:\\s*\r?\n(?://)?)?"
+      "(?:%(inner)s)"
+      "\\s*"
+      "\\*/"
+      "|"
+      "//(?:%(inner)s)"
+    ")"
+    "\\s*$") % {'inner': r"""[#@] sourceMappingURL=([^\s'"]*)"""})
 
 
 class UglifyJSCompressor(SubProcessCompressor):
@@ -10,3 +31,27 @@ class UglifyJSCompressor(SubProcessCompressor):
         if self.verbose:
             command += ' --verbose'
         return self.execute_command(command, js)
+
+     def compress_js_with_source_map(self, paths):
+        # Build a list of arguments to run, starting with the binary path and any
+        # user-specified arguments/flags.
+        args = re.split(r'\s+', settings.PIPELINE_UGLIFYJS_BINARY)
+        if settings.PIPELINE_UGLIFYJS_ARGUMENTS:
+            if isinstance(settings.PIPELINE_UGLIFYJS_ARGUMENTS, six.string_types):
+                args += shlex.split(settings.PIPELINE_UGLIFYJS_ARGUMENTS)
+            else:
+                args += settings.PIPELINE_UGLIFYJS_ARGUMENTS
+
+        args += ["--source-map", temp_file.name]
+        args += ["--source-map-include-sources"]
+        for path in abs_paths:
+            args += [path]
+
+        js = self.execute_command(args, None, shell=False)
+
+        with open(temp_file.name) as f:
+            source_map = f.read()
+
+        temp_file.close()
+
+        return js, source_map
